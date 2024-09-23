cbfed3aff50577162810ba75cf3f9a47653752f9
rotieno <rabinyitzahck@gmail.com>
2024-09-23 15:11:29 +0300

    feat: add more structs for holding the fetched data

--- api/structures.go
+++ api/structures.go
@@ -16,3 +16,31 @@ type ErrorContent struct {
 	Message string
 	Code         string
 }
+
+type Location struct{
+	Locations []string `json:"locations"`
+}
+
+type Date struct{
+	Dates []string `json:"dates"`
+}
+
+type Relations struct{
+	DatesLocation map[string][]string `json:"datesLocations"`
+}
+
+type Details struct{
+	ID int `json:"id"`
+	Image string `json:"image"`
+	Name string `json:"name"`
+	Members []string `json:"members"`
+	CreationDate int `json:"creationDate"`
+	FirstAlbum string `json:"firstAlbum"`
+}
+
+type AllDetails struct{
+	Details Details
+	Dates Date
+	Location Location
+	Relations Relations
+}
