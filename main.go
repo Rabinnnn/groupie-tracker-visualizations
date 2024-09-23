cd8ea431b2fafb82f39356e13719c02e128b800c
rotieno <rabinyitzahck@gmail.com>
2024-09-23 13:53:12 +0300

    feat: implement the display pages

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import(
 
 func main(){
 	http.HandleFunc("/", handlers.IndexHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	fmt.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
